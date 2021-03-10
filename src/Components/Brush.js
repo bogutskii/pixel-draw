cb8c128ac0daa9fa111e5f12b488a552e01ff9ba
bogutskii <sl9site@gmail.com>
2021-03-10 00:15:14 -0800

    -vertical brush

--- src/Components/Brush.js
+++ src/Components/Brush.js
@@ -8,10 +8,11 @@ const Brush = (props)=>{
     const {changeBrush} = props
     return <div className='brush-block'>
 
-        <button className='btn-brush' onClick={()=> changeBrush('dot')}>⊡</button>
-        <button className='btn-brush' onClick={()=> changeBrush('horizontal')}>⭤</button>
-        <button className='btn-brush' onClick={()=> changeBrush('vertical')}>⭥</button>
+        <button className='btn-brush' onClick={()=> changeBrush('dot')}>⊡ ▣</button>
+        <button className='btn-brush' onClick={()=> changeBrush('horizontal')}>↔</button>
+        <button className='btn-brush' onClick={()=> changeBrush('vertical')}>↕</button>
         <button className='btn-brush' onClick={()=> changeBrush('cross')}>✚</button>
+        <button className='btn-brush' onClick={()=> changeBrush('random')}>Fill ▩</button>
         <button className='btn-brush' onClick={()=> changeBrush('random')}>Fill random ⊞</button>
 
     </div>
--- src/Components/redux/reducers.js
+++ src/Components/redux/reducers.js
@@ -9,16 +9,13 @@ const initialState = {
 }
 
 
-
-
-
 const drawField = (state = initialState, action) => {
     switch (action.type) {
         case 'CHANGE_CURRENT_COLOR' :
             return {
                 ...state, currentColor: action.payload.color
             }
-
+// ******** START DRAW FUNCTIONAL **********************
         case 'CHANGE_PIXEL_COLOR_AND_SAVE_TO_HISTORY':
             let copyHistoryColor = [...state.historyColor]
             if (!copyHistoryColor.includes(state.currentColor)) {
@@ -29,25 +26,60 @@ const drawField = (state = initialState, action) => {
                 let range = [0, 9];
                 if (brush === 'horizontal') {
                     if (index > 9) {
-
-                        let r1 = Math.floor(index/ 10) // 9
-                        range = [r1*10 , r1*10+9]
+                        let r1 = Math.floor(index / 10) // 9
+                        range = [r1 * 10, r1 * 10 + 9]
                     }
+                } else if (brush === 'vertical') {
+                    let chekList = []
+                    let part = 10
+                    if (size === 400) {
+                        part = 20
+                    } else if (size === 1600) {
+                        part = 40
+                    }
+                    let p1 = index%part
+                    for (let i = 0; i < part; i++) {
+    chekList.push(p1+ (i * part))
+                    }
+                    return array.map((el, i) => chekList.includes(i) ? {
+                        ...el,
+                        color: state.currentColor
+                    } : el)
                 }
-                return array.map((el,i)=> (i>= range[0] && i <= range[1])? {
+
+
+// 100 -10
+//40 - 20
+//1600 - 40
+// 3 13  23 33
+// 43 53 63 73
+// 83 93 103 113
+
+
+                else {
+                    range = [index, index]
+                }
+
+
+                return array.map((el, i) => (i >= range[0] && i <= range[1]) ? {
                     ...el,
                     color: state.currentColor
-                } : el )
+                } : el)
             }
+
+
             // let copyField = [...state.field].map((el, i) => i === action.payload.index ? {
             //     ...el,
             //     color: state.currentColor
             // } : el)
-            let copyField = brushFill(action.payload.index, state.brush,state.currentSize,[...state.field])
+            let copyField = brushFill(action.payload.index, state.brush, state.currentSize, [...state.field])
 
             return {
                 ...state, field: copyField, historyColor: copyHistoryColor
             }
+
+
+        // end draw brush
         case 'CLEAR_FIELD':
 
             return {
